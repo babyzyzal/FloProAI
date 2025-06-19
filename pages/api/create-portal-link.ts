237bee1373590451a712fd9675eee6ca2d7c01e5
babyzyzal <jkbear08@gmail.com>
2025-06-19 17:20:01 -0600

    Fix Stripe API version in create-portal-link

--- pages/api/create-portal-link.ts
+++ pages/api/create-portal-link.ts
@@ -4,7 +4,7 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import Stripe from 'stripe'
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2023-08-16',
+  apiVersion: '2025-05-28.basil',
 })
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
